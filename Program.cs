302036231313a9e18af6da2a85db45a418a1b612
maduuuuuuuuuuuuu <e107ti@etec.sp.gov.br>
2023-10-27 20:41:34 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+int numeropar = 0;
+do
+{
+    Console.Write("Digite um numero inteiro e positivo: ");
+    Int32.TryParse(Console.ReadLine(), out numero);
+}
+while (numero <= 0);
+
+while (numeropar <= numero)
+{
+    Console.Write($"{numeropar} ");
+    numeropar = numeropar + 2;
+}
